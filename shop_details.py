a1a457d3cd2d42c0b3e1ded8e39a2002a9f4b889
Eranda Sanjaya <76166067+ES-3508@users.noreply.github.com>
2025-03-25 13:39:57 +0530

    Fix: Chrome driver

--- shop_details.py
+++ shop_details.py
@@ -52,6 +52,7 @@ def setup_driver():
         options = Options()
         
         # Linux-specific options
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
